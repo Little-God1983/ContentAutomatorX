f49b0f7703331c2f22e8c8869446e69d558c0cf5
Chris <little-god@web.de>
2026-07-12 19:05:00 +0200

    chore: scaffold solution (Domain/Application/Infrastructure/Web + tests)

---
+++ tests/ContentAutomatorX.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ContentAutomatorX.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/ContentAutomatorX.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace ContentAutomatorX.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
